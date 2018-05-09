74fdb3eecd0c693e809bea8039843bd87a638ab8
Karol <karol.wawer.pl@gmail.com>
2018-05-09 23:41:28 +0200

    dodanie pliku z zadaniem

---
+++ main.js
@@ -0,0 +1,9 @@
+function sprawdz(n) {
+    var n = 140;
+    if (n >= 100 && n <= 200) {
+        console.log("Liczba znajduje się w przedziale");
+    } else {
+        console.log("Liczba nie znajduje się w przedziale");
+    }
+}
+sprawdz();
